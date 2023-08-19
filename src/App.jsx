c3fd5c7090fd586d9059f7ebebf120ba2436fe25
Akhilesh m t <akhileshthykkat843@gmail.com>
2023-08-19 13:48:56 +0530

    UserChart

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserChart from './components/UserChart';
+import AverageChart from './components/AverageChart';
+const App = () => {
+  return (
+    <>
+      <AverageChart />
+    </>
+  );
+};
+
+export default App;
